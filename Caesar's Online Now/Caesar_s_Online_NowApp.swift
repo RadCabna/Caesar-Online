38e69581c1116a4f2e3ce2ad76f40240a2c943c9
Radcabna <radcabna@gmail.com>
2025-06-09 18:08:51 +0300

    Initial Commit

---
+++ Caesar's Online Now/Caesar_s_Online_NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Caesar_s_Online_NowApp.swift
+//  Caesar's Online Now
+//
+//  Created by Алкександр Степанов on 09.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Caesar_s_Online_NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
